35505831522f1b7a63e32d518824ba53a39527e1
Jordan Nelson <jordannelson@protonmail.com>
2020-02-12 14:23:38 -0600

    Add static utility class Detect to library

---
+++ Assets/rootutils-unitycsharp/Runtime/Detect.cs
@@ -0,0 +1,49 @@
+public static class Detect {
+        public static List<GameObject> DetectToList(GameObject origin, float radius) {
+            List<GameObject> result = new List<GameObject>();
+            Collider[] detected = Physics.OverlapSphere(origin.transform.position, radius);
+
+            foreach (Collider col in detected) {
+                if (col.gameObject != origin) {
+                    result.Add(col.gameObject);
+                }
+            }
+
+            return result;
+        }
+
+        public static GameObject[] DetectToArray(GameObject origin, float radius) {
+            return DetectToList(origin, radius).ToArray();
+        }
+
+        public static T DetectClosestWithComponent<T>(GameObject origin, float radius) where T : Component {
+            List<GameObject> detectedObjs = DetectToList(origin, radius);
+            Distance.SortObjectsByDistance(origin, detectedObjs);
+
+            foreach (GameObject obj in detectedObjs) {
+                foreach (Component comp in obj.GetComponents<Component>()) {
+                    if (comp is T) {
+                        return (T)comp;
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        public static List<T> DetectAllWithComponent<T>(GameObject origin, float radius) where T : Component {
+            List<GameObject> detectedObjs = DetectToList(origin, radius);
+            Distance.SortObjectsByDistance(origin, detectedObjs);
+            List<T> result = new List<T>();
+
+            foreach (GameObject obj in detectedObjs) {
+                foreach (Component comp in obj.GetComponents<Component>()) {
+                    if (comp is T) {
+                        result.Add((T)comp);
+                    }
+                }
+            }
+
+            return result;
+        }
+    }   
