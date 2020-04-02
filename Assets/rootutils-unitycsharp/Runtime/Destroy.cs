744f692dfe0f9437af31c75c9e8b1adb4bcbfff8
Jordan Nelson <jordannelson@protonmail.com>
2020-04-02 14:48:33 -0500

    fix defect. DestroyAll should be static

--- Assets/rootutils-unitycsharp/Runtime/Destroy.cs
+++ Assets/rootutils-unitycsharp/Runtime/Destroy.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 
 public static class Destroy {
-    public void DestroyAll<T>() where T : UnityEngine.MonoBehaviour {
+    public static void DestroyAll<T>() where T : UnityEngine.MonoBehaviour {
         foreach(T t in GameObject.FindObjectsOfType<T>()) {
             GameObject.Destroy(t.transform.gameObject);
         }
